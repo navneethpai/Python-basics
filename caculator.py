0de1994ecd7dfa547aaf016090b68ea0551e8e5f
Nk daaa <34378434+navneethpai@users.noreply.github.com>
2018-10-11 20:44:38 +0530

    Add files via upload

---
+++ caculator.py
@@ -0,0 +1,28 @@
+#Program for creating an calculator
+def _add(a,b):
+	return a+b
+def _sub(a,b):
+	return a-b
+def _multi(a,b):
+	return a*b
+def _dev(a,b):
+	return a/b
+def _exp(a):
+	return 2.71828**a
+print "MENU\n____\n1.Addition\n2.Subtraction\n3.Multiply\n4.Division\n5.Expotinal"
+n=input("Enter the choice:")
+x=0
+if n!=5:
+	a,b=input("Enter the numbers for operations:")
+	if n==1:
+		x=_add(a,b)
+	elif n==2:
+		x=_sub(a,b)
+	elif n==3:
+		x=_multi(a,b)
+	elif n==4:
+		x=_dev(a,b)
+else:
+        a=input("Enter the number:")
+        x=_exp(a)
+print x
