2f87fd7d266328d45b60f75a5c64cea55345674a
Nk daaa <34378434+navneethpai@users.noreply.github.com>
2018-11-24 11:43:51 +0530

    Add files via upload

--- file2.py
+++ file2.py
@@ -1,14 +1,13 @@
 n=raw_input('Enter the str:')
 try:
-	f=open('tx.txt','r+')
+	f=open('tx.txt','w')
 	f.write(n)
-finally IOError:
+except IOError:
 	print 'Fails'
+f.close()
+f=open('tx.txt','r')
 for i in f.read():
 	if i==' ':
-		f.write('-')
-f.close()
-f=open ("tx.txt",'r') 
-for j in f.readlines():
-	print j	
-f.close()
+		print '-',
+	else:
+		print i,	
